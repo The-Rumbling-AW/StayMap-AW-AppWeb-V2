f8bc09d658d3ef0dd58a82ea06261cc7294f8d42
aria <168163983+ariaalizz@users.noreply.github.com>
2025-06-21 01:53:57 -0500

    Delete src/shared directory

--- src/shared/services/http.instance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const httpInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-});
-
-export default httpInstance;
