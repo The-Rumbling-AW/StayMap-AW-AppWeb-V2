610e609dc3d40093988d7e9c9ea1381d923b5fe0
Antuanete01 <u202310358@upc.edu.pe>
2025-06-22 16:17:30 -0500

    Update post.service.js

--- src/community/services/post.service.js
+++ src/community/services/post.service.js
@@ -1,4 +1,4 @@
-import httpInstance from "../../../../../../Desktop/pruebitatb/src/shared/services/http.instance.js";
+import httpInstance from "@/shared/services/http.instance.js";
 
 /**
  * @class PostService
